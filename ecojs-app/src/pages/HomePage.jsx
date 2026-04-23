493f11963ed10bb4fda54393a93cadbacbc3028d
rollingnomad <rollingnomadgames@proton.me>
2026-04-23 06:28:43 -0400

    Basic Site addition to db

--- ecojs-app/src/pages/HomePage.jsx
+++ ecojs-app/src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import { SearchBar } from "../components/SearchBar";
+import { SiteCommunityBox } from "../components/SiteCommunityBox";
 
 export function HomePage() {
   return (
     <div className="w-full">
+      <SiteCommunityBox />
       <SearchBar />
     </div>
   );
